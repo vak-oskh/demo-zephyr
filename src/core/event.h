ccf1289ddcc74e21a82330f635f2676358519320
Vyacheslav Khudan <vyacheslav.oskh@gmail.com>
2025-03-23 20:05:11 -0600

    Add a skeleton for application events

---
+++ src/core/event.h
@@ -0,0 +1,41 @@
+#ifndef CORE_EVENT_H
+#define CORE_EVENT_H
+
+#include <zephyr/kernel.h>
+
+/**
+ * Describes events as an item in comminication queue.
+ */
+#define GRP_OFFSET(grp)     (grp << 8)
+
+// Events from external devices to handle by the controller
+#define EXT_EVENTS          GRP_OFFSET(BIT(0))          // 0x100
+
+// Events for the controller from GUI to take action.
+#define GUI_CTRL_EVENTS     GRP_OFFSET(BIT(1))
+
+// Events for the GUI from controler to take action.
+#define CTRL_GUI_EVENTS     GRP_OFFSET(BIT(2))          // 0x400
+
+typedef struct {
+    void *fifo_reserved;   /* Reserved for use by FIFO */
+
+    uint16_t code;
+    void *data;
+    uint8_t __align[2];     /* FIFO's items require alignment N+4 */
+} event_t;
+
+__attribute__((always_inline))
+inline void free_event(event_t **event)
+{
+    if (event && *event)
+    {
+        if ((*event)->data)
+            k_free((*event)->data);
+
+        k_free(*event);
+        *event = NULL;
+    }
+}
+
+#endif /* CORE_EVENT_H */
