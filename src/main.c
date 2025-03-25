24645b35b48a00d77110e8b94a11b949dc105859
Vyacheslav Khudan <vyacheslav.oskh@gmail.com>
2025-03-25 11:42:14 -0600

    Update the main application file to run processing threads

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
-#include <stdio.h>
+#include "gui/gui.h"
+#include "controller/controller.h"
+#include "emulators/manager.h"
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(app_demo, CONFIG_APP_LOG_LEVEL);
 
 int main()
 {
-    printf("Hello My World! %s\n", CONFIG_BOARD_TARGET);
+    /* Initialization stage */
+    emulators_start();
+    controller_start();
+    gui_start();
 
-	return 0;
+    LOG_INF("The board '%s' is started", CONFIG_BOARD_TARGET);
+
+    /* Application waiting stage as other processing threads are running */
+    while(true)
+    {
+        k_sleep(K_FOREVER);
+    }
+
+    LOG_INF("Stop %s\n", CONFIG_BOARD_TARGET);
+
+    /* Shutdown stage */
+    gui_stop();
+    controller_stop();
+    emulators_stop();
+
+    return 0;
 }
